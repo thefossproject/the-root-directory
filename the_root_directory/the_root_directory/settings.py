7c19a7adffd9b1b6d314016c62a9610c915075ff
garzuzo <garzuxo@gmail.com>
2026-03-24 12:00:06 -0300

    feat(admin): add enable admin

--- the_root_directory/the_root_directory/settings.py
+++ the_root_directory/the_root_directory/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
+ENABLE_ADMIN = os.environ.get("ENABLE_ADMIN", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_TRUSTED_ORIGINS", "").split(",")
